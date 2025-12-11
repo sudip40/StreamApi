328356379daedc64db95a871c8eb6c77eb2ae852
Sudip Karmakar <karmakar.sudip40@gmail.com>
2025-12-11 13:11:58 +0530

    Day 1

--- src/Basic/GenerateStream.java
+++ src/Basic/GenerateStream.java
@@ -1,5 +1,14 @@
 package Basic;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class GenerateStream {
 //    Generate a stream of 10 random integers.
+public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(5, 3, 9, 1, 4, 3, 8, 6, 5, 9, 2, 7);
+    Stream<Integer> stream = list.stream();
+    System.out.println(stream);
+}
 }
--- src/Basic/RemoveDuplicates.java
+++ src/Basic/RemoveDuplicates.java
@@ -1,5 +1,17 @@
 package Basic;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RemoveDuplicates {
 //    Remove duplicate elements from a list.
+public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(1, 1, 2, 3, 5, 3, 2, 8, 9, 9, 10, 8, 7, 7);
+    // using set
+    list.stream().collect(Collectors.toSet()).stream().forEach(e-> System.out.println(e));
+
+    // using distinct
+    list.stream().distinct().forEach(e-> System.out.println(e));
+}
 }
