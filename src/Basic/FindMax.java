ec72ec7b8bfb8886b649cb2fd884774340ac2caf
Sudip Karmakar <karmakar.sudip40@gmail.com>
2025-12-11 13:27:45 +0530

    max min

--- src/Basic/FindMax.java
+++ src/Basic/FindMax.java
@@ -1,5 +1,23 @@
 package Basic;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class FindMax {
 //    Find max value from a list using Stream.
+public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(1, 1, 2, 3, 5, 3, 2, 8, 9, 9, 10, 8, 7, 7);
+
+    int i = list.stream().mapToInt(e->e).max().orElseThrow();
+
+    Integer oi = list.stream().max((a, b)->a-b).get();
+
+    Integer oi1 = list.stream().max((a,b)->a.compareTo(b)).get();
+
+    Integer oi2 = list.stream().max(Comparator.comparingInt(e->e)).get();
+
+    System.out.println(i+" "+oi+" "+oi1+" "+oi2);
+}
 }
--- src/Basic/FindMin.java
+++ src/Basic/FindMin.java
@@ -1,5 +1,22 @@
 package Basic;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class FindMin {
 //    Find min value from a list using Stream.
+public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(1, 1, 2, 3, 5, 3, 2, 8, 9, 9, 10, 8, 7, 7);
+
+    int a = list.stream().mapToInt(e->e).min().orElseThrow();
+
+    Integer b = list.stream().min((x,y)->x-y).get();
+
+    Integer c = list.stream().min(Comparator.comparingInt(x->x)).get();
+
+    Integer d = list.stream().min((x,y)->x.compareTo(y)).get();
+
+    System.out.println(a+" "+b+" "+c+" "+d);
+}
 }
