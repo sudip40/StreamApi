9f8114f179efd680013636b50b97a5cb131b48a7
Sudip Karmakar <karmakar.sudip40@gmail.com>
2025-12-11 14:03:09 +0530

    sorted string length

---
+++ src/Basic/StringLengthSorting.java
@@ -0,0 +1,19 @@
+package Basic;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringLengthSorting {
+//    Sort the list in ascending order of string length
+//    Sort the list in descending order of string length
+public static void main(String[] args) {
+    List<String> names = Arrays.asList(
+            "Sudip", "Rahul", "Amit", "Jonathan", "Li", "Christopher", "Sid"
+    );
+    List<String> resAsc = names.stream().sorted((a, b)->a.length()-b.length()).collect(Collectors.toList());
+    resAsc.stream().forEach(e-> System.out.println(e));
+    List<String> resDesc =names.stream().sorted((a,b)->b.length()-a.length()).collect(Collectors.toList());
+    resDesc.stream().forEach(e-> System.out.println(e));
+}
+}
