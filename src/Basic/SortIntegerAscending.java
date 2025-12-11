5a3a9eefcb57f11029924c7dd91ed24cc9365848
Sudip Karmakar <karmakar.sudip40@gmail.com>
2025-12-11 13:53:07 +0530

    sorted

--- src/Basic/SortIntegerAscending.java
+++ src/Basic/SortIntegerAscending.java
@@ -1,5 +1,15 @@
 package Basic;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SortIntegerAscending {
 //    Sort a list of integers in ascending order.
+public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(1, 1, 2, 3, 5, 3, 2, 8, 9, 9, 10, 8, 7, 7);
+    List<Integer> res = list.stream().sorted((a,b)->a-b).collect(Collectors.toList());
+    res.stream().forEach(e-> System.out.println(e));
+}
 }
--- src/Basic/SortIntegerDescending.java
+++ src/Basic/SortIntegerDescending.java
@@ -1,5 +1,14 @@
 package Basic;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SortIntegerDescending {
 //    Sort a list of strings in descending order.
+public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(1, 1, 2, 3, 5, 3, 2, 8, 9, 9, 10, 8, 7, 7);
+    List<Integer> res = list.stream().sorted((a,b)->b-a).collect(Collectors.toList());
+    res.stream().forEach(e-> System.out.println(e));
+}
 }
